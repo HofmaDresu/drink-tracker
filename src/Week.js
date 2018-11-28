a55035fdbdbb5ad9059b9ba90c70831bc7e3b565
Matt Hoffman <hofmadresu@gmail.com>
2018-11-28 11:35:30 -0500

    show total drinks

--- src/Week.js
+++ src/Week.js
@@ -4,13 +4,19 @@ import './Week.css';
 
 export default function Week(props) {
     const drinks = props.days.reduce((acc, d) => acc + d.drinks, 0);
-    const weekDrinkHealth = drinks >= 14 ? "bad" : drinks > 10 ? "moderate" : "good";
+    const weekDrinkHealth = (drinks >= 14 ? "bad" : drinks > 10 ? "moderate" : "good");
     const weekClasses = ["week", weekDrinkHealth].join(" ");
+    const weekTotalClasses = ["week-total", weekDrinkHealth].join(" ");
 
     const dayComponents = props.days.map(d => <Day day={d} key={d.date} onChangeDrinks={(drinks) => props.onChangeDrinksForDay(d.date, drinks)} />);
     return (
-        <div className={weekClasses}>
-            {dayComponents}
+        <div className="week-container">
+            <div className={weekClasses}>
+                {dayComponents}
+            </div>
+            <span className={weekTotalClasses}>
+                Total: {drinks} drinks
+            </span>
         </div>
     );
 }
