6cf10d4e5aa5795d0b5f59aaa9c3d0721ea1139e
Matt Hoffman <hofmadresu@gmail.com>
2018-11-28 11:28:09 -0500

    Updates

---
+++ precache-manifest.08996ff0e41579c57982981160bdcb2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b7805569b9ced62c08c",
+    "url": "/drink-tracker/static/js/runtime~main.2b780556.js"
+  },
+  {
+    "revision": "153b00d5ea4a7ca13436",
+    "url": "/drink-tracker/static/js/main.153b00d5.chunk.js"
+  },
+  {
+    "revision": "2cdbf539c2ee88362256",
+    "url": "/drink-tracker/static/js/1.2cdbf539.chunk.js"
+  },
+  {
+    "revision": "153b00d5ea4a7ca13436",
+    "url": "/drink-tracker/static/css/main.cc707f07.chunk.css"
+  },
+  {
+    "revision": "db3984cf0e4a1d35a1ed01831409a43e",
+    "url": "/drink-tracker/index.html"
+  }
+];
